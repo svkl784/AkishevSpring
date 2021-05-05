633d2974bf7f362e4a0095f46ec7e3e7e63501ec
Nataliya Svirid <klybiksvirid@gmail.com>
2021-05-05 22:04:35 +0300

    1

---
+++ src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.awt.print.PrinterIOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicPlayer {
+    private Music music;
+
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    // IoC
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public MusicPlayer() {}
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
+    }
+}
---
+++ src/main/java/org/example/RapMusic.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class RapMusic implements Music{
+
+    @Override
+    public String getSong() {
+        return "50 cent";
+    }
+}
